769332cea7ae56c0f8ec3bc787f4967e83c17825
Nestor Kauil Xix <neto1xix@gmail.com>
2019-05-28 16:44:04 -0500

    [FEATURE] Actualizado configuracion del nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'Nestor Kauil Xix - Mi portafolio',
+    title: 'Nestor Kauil Xix - Mi Portafolio',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -37,6 +37,7 @@ module.exports = {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    { src: '~plugins/ga.js', ssr: false }
   ],
 
   /*
