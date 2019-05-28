5fb34ae92657cb76e16a4958f28597693523a1fd
Nestor Kauil Xix <neto1xix@gmail.com>
2019-05-28 16:47:10 -0500

    [FEATURE] Agregado al index componente de Hero

--- pages/index.vue
+++ pages/index.vue
@@ -2,15 +2,18 @@
   <div>
     <Navbar />
     <Header />
+    <Hero />
   </div>
 </template>
 
 <script>
 import Navbar from '@/layouts/Navbar.vue'
 import Header from '@/layouts/Header.vue'
+import Hero from '@/layouts/Hero.vue'
 
 export default {
   components: {
+    Hero,
     Header,
     Navbar
   }
