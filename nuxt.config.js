e8df5e6deff9a94c575a503a65c4c176e15abf3d
Nestor Kauil Xix <neto1xix@gmail.com>
2019-06-16 13:41:46 -0500

    [FEATURE] Agregado configuracion de plugins nuevos, axios, scrollit, vuealert. Fuentes nuevas y configuracion global de la URL de la API

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,23 +1,29 @@
+const webpack = require('webpack')
 const pkg = require('./package')
-const webpack = require("webpack")
 
 module.exports = {
   mode: 'spa',
-
   /*
   ** Headers of the page
   */
+
+  env: {
+    API_URL: (process.env.NODE_ENV !== 'production') ? 'http://localhost:5000' : 'https://nestorkx-server.herokuapp.com'
+  },
+
   head: {
     title: 'Nestor Kauil Xix - Mi Portafolio',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'msvalidate.01', content: 'DFCDFA848CD9AB7E04857719D435334C' },
+      { name: 'google-site-verification', content: 'ZeAWIyHkTzIMZkDyAhQtfITffvSsUqQRD2fIWyTwIhg' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800,900' },
-      { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.8.2/css/all.css', integrity:'sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay', crossorigin:'anonymous' },
+      { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.8.2/css/all.css', integrity: 'sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay', crossorigin: 'anonymous' },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
@@ -37,7 +43,9 @@ module.exports = {
   ** Plugins to load before mounting the App
   */
   plugins: [
-    { src: '~plugins/ga.js', ssr: false }
+    { src: '~plugins/ga.js', ssr: false },
+    { src: "~plugins/axios.js", ssr: false },
+    { src: "~plugins/scrollIt.js", ssr: false }
   ],
 
   /*
@@ -48,7 +56,8 @@ module.exports = {
     '@nuxtjs/axios',
     // Doc: https://bootstrap-vue.js.org/docs/
     // 'bootstrap-vue/nuxt',
-    '@nuxtjs/pwa'
+    '@nuxtjs/pwa',
+    ['vue-sweetalert2/nuxt', { includeCss: false }]
   ],
   /*
   ** Axios module configuration
@@ -67,7 +76,8 @@ module.exports = {
     vendor: ['jquery'],
     plugins: [
       new webpack.ProvidePlugin({
-        $: 'jquery'
+        $: 'jquery',
+        jQuery: 'jquery'
       })
     ],
     extend(config, ctx) {
